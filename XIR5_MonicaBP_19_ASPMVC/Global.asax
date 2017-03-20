4a20f2a77567c6b8a22653cc25daf57b0059919c
Monica BP <Monica BP@DESKTOP-F1DJJMQ>
2017-03-19 21:07:19 -0700

    Add project files.

---
+++ XIR5_MonicaBP_19_ASPMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="XIR5_MonicaBP_19_ASPMVC.MvcApplication" Language="C#" %>
---
+++ XIR5_MonicaBP_19_ASPMVC/Security/AuthorizeRoleAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XIR5_MonicaBP_19_ASPMVC.Models.DB;
+using XIR5_MonicaBP_19_ASPMVC.Models.EntityManager;
+
+namespace XIR5_MonicaBP_19_ASPMVC.Security
+{
+    public class AuthorizeRolesAttribute : AuthorizeAttribute
+    {
+        private readonly string[] userAssignedRoles; public AuthorizeRolesAttribute(params string[] roles)
+        {
+            this.userAssignedRoles = roles;
+        }
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            bool authorize = false; using (DemoDBEntities db = new DemoDBEntities())
+            {
+                UserManager UM = new UserManager();
+                foreach (var roles in userAssignedRoles)
+                {
+                    authorize = UM.IsUserInRole(httpContext.User.Identity.Name, roles);
+                    if (authorize)
+                        return authorize;
+                }
+            }
+
+            return authorize;
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectResult("~/Home/UnAuthorized");
+        }
+    }
+}
